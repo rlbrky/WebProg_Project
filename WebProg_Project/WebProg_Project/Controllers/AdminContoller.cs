8fd03790d070f86f87d6256ccaadd6fbe68d4211
rlBrky <rlbrky@users.noreply.github.com>
2021-12-21 23:48:20 +0300

    Admin Panel Template

---
+++ WebProg_Project/WebProg_Project/Controllers/AdminContoller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebProg_Project.Controllers
+{
+    public class AdminContoller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
