f4f83dd57f4c9650c480559bb769f006398ecdba
RafetErsoy <rafet.ersoy.cs@gmail.com>
2021-12-26 17:56:19 +0300

    SummonerController.cs created and filled
    "Controllers/SummonerController.cs" created "Views/Summoners" and sub files "Create.cshtml", "Delete.cshtml", "Details.cshtml", "Edit.cshtml", "Index.cshtml" are created

---
+++ ProjectWebProg/ProjectWebProg/Controllers/SummonersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjectWebProg.Data;
+using ProjectWebProg.Models;
+
+namespace ProjectWebProg.Controllers
+{
+    public class SummonersController : Controller
+    {
+        private readonly ProjectWebProgContext _context;
+
+        public SummonersController(ProjectWebProgContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Summoners
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Summoner.ToListAsync());
+        }
+
+        // GET: Summoners/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var summoner = await _context.Summoner
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (summoner == null)
+            {
+                return NotFound();
+            }
+
+            return View(summoner);
+        }
+
+        // GET: Summoners/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Summoners/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AccountId,PuuId,Name,ProfileIconId,RevisionDate,SummonerLevel")] Summoner summoner)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(summoner);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(summoner);
+        }
+
+        // GET: Summoners/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var summoner = await _context.Summoner.FindAsync(id);
+            if (summoner == null)
+            {
+                return NotFound();
+            }
+            return View(summoner);
+        }
+
+        // POST: Summoners/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,AccountId,PuuId,Name,ProfileIconId,RevisionDate,SummonerLevel")] Summoner summoner)
+        {
+            if (id != summoner.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(summoner);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SummonerExists(summoner.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(summoner);
+        }
+
+        // GET: Summoners/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var summoner = await _context.Summoner
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (summoner == null)
+            {
+                return NotFound();
+            }
+
+            return View(summoner);
+        }
+
+        // POST: Summoners/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var summoner = await _context.Summoner.FindAsync(id);
+            _context.Summoner.Remove(summoner);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SummonerExists(string id)
+        {
+            return _context.Summoner.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ProjectWebProg/ProjectWebProg/Data/ProjectWebProgContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectWebProg.Models;
+
+namespace ProjectWebProg.Data
+{
+    public class ProjectWebProgContext : DbContext
+    {
+        public ProjectWebProgContext (DbContextOptions<ProjectWebProgContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjectWebProg.Models.Summoner> Summoner { get; set; }
+    }
+}
