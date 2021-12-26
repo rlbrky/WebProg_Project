c29656ceb1059bc93dceb89f6b212c9567d29fab
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2021-12-26 03:32:30 +0300

    Necessary actions related to "Summoner" have been performed (#1)
    1- Models/Summoner.cs created and filled
    2- Controllers/SummonersController.cs created and filled
    3- Migrations added and databases updated as a result, the specified folders and sub-files were created.
    	3.1- Data folders created
    		3.1.1- Data/WebProg_ProjectContext.cs created and filled
    	3.2- Migrations folders created
    		3.2.1- Migrations/20211225234114_InitialCreate.cs created and filled
    		3.2.2- Migrations/WebProg_ProjectContextModelSnapshot.cs created and filled
    4- SeedDataSummoner.cs created and filled
    5- Program.cs/Main function edited
    6- Added "Summoners" button to file Views/Shared/_Layout.cshtml

---
+++ WebProg_Project/WebProg_Project/Models/SeedDataSummoner.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebProg_Project.Data;
+using System;
+using System.Linq;
+
+namespace WebProg_Project.Models
+{
+    public static class SeedDataSummoner
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new WebProg_ProjectContext(
+                serviceProvider.GetRequiredService<
+                DbContextOptions<WebProg_ProjectContext>>()))
+            {
+                // Look for any summoners
+                if (context.Summoner.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+                context.Summoner.AddRange(
+                    new Summoner
+                    {
+                        //Id = "4FElUEm8R_zf4CxTTZyKKMSs8rUEmZBySfduPWVP19tuBQ",
+                        //AccountId = "mACiCUShtXyCLXv6s8SH8pmjyxKolDgqC00PZDbbYxgB-so",
+                        //PuuId = "VI4hvF05YoUiWu92R-njLaJfS1m5ZWs-Pgt4-clVZSRLwu7YIrDEQW_wPmrjN3fyNdjz8U7sKO2jgg",
+                        Name = "pistipist",
+                        ProfileIconId = 1386,
+                        RevisionDate = Convert.ToDateTime(1640381656000),
+                        SummonerLevel = 245
+                    },
+
+                    new Summoner
+                    {
+                        //Id = "aVR0HD9zmWqza1X1amRtDjrFpOYpicGylDjV6qUDONdY1g",
+                        //AccountId = "8VZzvytuZM30yJUgM19Wgp5AiKfWuIyyy2zlWf2cMyE7-OQ",
+                        //PuuId = "9MnqcW4RzyiXLcTlWFidAFTd-nLmJCS8M-TfLIvvamLNy2aOoesDy-pBCJMVl2hoAQG_OhRTrkMSJA",
+                        Name = "Obskurität",
+                        ProfileIconId = 4631,
+                        RevisionDate = Convert.ToDateTime(1640383826000),
+                        SummonerLevel = 273
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
